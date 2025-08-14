669a1abc44317ef9d3f43880493d26f783a1aa37
Agustin <am.motos@hotmail.com>
2025-08-14 09:10:32 -0300

    Cars

--- components/landing/header/Header.tsx
+++ components/landing/header/Header.tsx
@@ -11,7 +11,7 @@ const Header: React.FC = () => {
           href="/"
           className="text-2xl font-bold text-zinc-800 dark:text-zinc-200"
         >
-          Bs.As Carss
+          Bs.As Cars
         </Link>
 
         <div className="flex items-center space-x-3">
